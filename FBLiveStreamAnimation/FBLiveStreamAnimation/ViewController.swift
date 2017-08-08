8f66c6fd96339b820260c3b3938e3612a777f6ff
Yashi Pallav <puneetgupta5294@gmail.com>
2017-08-08 11:01:38 +0530

    completed facebook's live stream animation

--- FBLiveStreamAnimation/FBLiveStreamAnimation/ViewController.swift
+++ FBLiveStreamAnimation/FBLiveStreamAnimation/ViewController.swift
@@ -14,18 +14,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let curvedView = CurvedView(frame: view.frame)
-        curvedView.backgroundColor = .yellow
+//        let curvedView = CurvedView(frame: view.frame)
+//        curvedView.backgroundColor = .yellow
+//        view.addSubview(curvedView)
+
         
         
-        let imageView = 
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    func handleTap() {
+        (0...10).forEach { (_) in
+            generateAnimatedViews()
+        }
+    }
+    
+    fileprivate func generateAnimatedViews() {
         
-        view.addSubview(curvedView)
+        let image = drand48() > 0.5 ? #imageLiteral(resourceName: "ha") : #imageLiteral(resourceName: "thumbsUp")
+        let imageView = UIImageView(image: image)
+        //let imageView1 = UIImageView(image: #imageLiteral(resourceName: "thumbsUp"))
+        let dimension = 20 + drand48() * 10
         
+        imageView.frame = CGRect(x: 0, y: 0, width: dimension, height: dimension)
+        //imageView1.frame = CGRect(x: 0, y: 0, width: dimension, height: dimension)
         
+        let animation = CAKeyframeAnimation(keyPath: "position")
         
+        animation.path = customPath().cgPath
+        animation.duration = 2 + drand48() * 3
+        animation.fillMode = kCAFillModeForwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        
+        imageView.layer.add(animation, forKey: nil)
+        //imageView1.layer.add(animation, forKey: nil)
+        
+        view.addSubview(imageView)
+        //view.addSubview(imageView1)
+
         
     }
+    
 
 }
 
@@ -34,11 +64,12 @@ func customPath() -> UIBezierPath {
     
     path.move(to: CGPoint(x: 0, y: 200))
     
-    let endPoint = CGPoint(x: 400, y: 200)
+    let endPoint = CGPoint(x: 450, y: 200)
     //path.addLine(to: endPoint)
     
-    let cp1 = CGPoint(x: 100, y: 100)
-    let cp2 = CGPoint(x: 200, y: 300)
+    let randomYShift = 200 + drand48() * 300
+    let cp1 = CGPoint(x: 100, y: 100 - randomYShift)
+    let cp2 = CGPoint(x: 200, y: 300 + randomYShift)
     path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
     return path
     
